2737f50be63f6238490c334b2d6cd29f0bd0bb0d
Jyotpreet Kaur <justechere@gmail.com>
2024-04-15 23:31:57 +0530

    create SDP offer

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let localStream;
 let remoteStream;
+let peerConnection;
 
 let init = async () => {
   try {
@@ -8,8 +9,23 @@ let init = async () => {
       audio: false,
     });
     document.getElementById("user-1").srcObject = localStream;
+
+    createOffer();
   } catch (error) {
     console.error("Error accessing media devices:", error);
   }
 };
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection();
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  console.log("Offer:", offer);
+};
+
 init();
