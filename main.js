536c25348c62da4db92e230e831c1931502ea12a
Jyotpreet Kaur <justechere@gmail.com>
2024-04-15 23:54:08 +0530

    set up stun server

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+};
+
 let init = async () => {
   try {
     localStream = await navigator.mediaDevices.getUserMedia({
@@ -17,10 +25,20 @@ let init = async () => {
 };
 
 let createOffer = async () => {
-  peerConnection = new RTCPeerConnection();
+  peerConnection = new RTCPeerConnection(servers);
 
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
